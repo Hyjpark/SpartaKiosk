4991aae0b0d5eaaa474873f04b138974337b57cb
Hyjpark <hyej042@gmail.com>
2025-07-16 16:46:08 +0900

    feat: MenuItem 클래스를 활용한 메뉴 리스트 구현
    - 메뉴 출력 시 StringBuilder를 활용하여 성능 최적화

--- src/main/java/com/example/kiosk/Main.java
+++ src/main/java/com/example/kiosk/Main.java
@@ -1,7 +1,29 @@
 package com.example.kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Kiost!");
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("ShackBuger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n[ SHAKESHACK MENU ]\n");
+        for (int  i = 0; i < menuItems.size(); i++) {
+            int blankLength = 12;
+            sb.append((i + 1) + ". " );
+            sb.append(menuItems.get(i).name);
+            sb.append(" ".repeat(blankLength - menuItems.get(i).name.length()) +  " | "); // 이름과 가격 사이의 공백 생성
+            sb.append("W " + menuItems.get(i).price +  " | ");
+            sb.append(menuItems.get(i).description);
+            sb.append("\n");
+        }
+        sb.append("0. 종료 \t| 종료");
+
+        System.out.println(sb.toString());
     }
 }
