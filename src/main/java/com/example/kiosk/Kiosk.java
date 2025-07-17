5336ebf83472f0fa7fbd0507951b68d8b1a22ca9
Hyjpark <hyej042@gmail.com>
2025-07-17 18:15:08 +0900

    feat: Menu가 가진 List<MenuItem> 메뉴 출력

--- src/main/java/com/example/kiosk/Kiosk.java
+++ src/main/java/com/example/kiosk/Kiosk.java
@@ -15,35 +15,51 @@ public class Kiosk {
     public void start() {
         Scanner sc =  new Scanner(System.in);
         boolean run = true;
-        //while (run) {
+        while (run) {
             try {
                 // 상위 카테고리 메뉴 출력
-                System.out.println("[ MAIN MENU ]");
+                System.out.println("\n[ MAIN MENU ]");
                 for (int i = 0; i < menus.size(); i++) {
                     System.out.println((i + 1) + ". " + menus.get(i).category);
                 }
                 System.out.println("0. 종료 \t| 종료");
 
-//                int selectMenu = sc.nextInt();
-//                int menuIndex = selectMenu - 1;
-//                switch (selectMenu) {
-//                    case 0:
-//                        System.out.println("프로그램을 종료합니다.");
-//                        run = false;
-//                        break;
-//                    case 1:
-//                    case 2:
-//                    case 3:
-//                    case 4:
-//                        System.out.print("선택한 메뉴 : ");
-//                        System.out.println(menuItems.get(menuIndex).name + " | W " + menuItems.get(menuIndex).price + " | " + menuItems.get(menuIndex).description);
-//                        break;
-//                    default:
-//                        throw new InputMismatchException("존재하지 않는 메뉴입니다.");
-//                }
+                int selectMenu = sc.nextInt();
+                Menu menu = null;
+                switch (selectMenu) {
+                    case 0:
+                        System.out.println("프로그램을 종료합니다.");
+                        run = false;
+                        break;
+                    case 1:  case 2: case 3:
+                        menu = menus.get(selectMenu - 1);
+                }
+
+                if (run) {
+                    // 메뉴 출력
+                    menu.showMenuItem();
+
+                    int selectMenuItem = sc.nextInt();
+                    int menuIndex = selectMenuItem - 1;
+                    switch (selectMenuItem) {
+                        case 0:
+                            continue;
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                            System.out.print("선택한 메뉴 : ");
+                            System.out.println(menu.menuItems.get(menuIndex).name + " | W "
+                                    + menu.menuItems.get(menuIndex).price
+                                    + " | " + menu.menuItems.get(menuIndex).description);
+                            break;
+                        default:
+                            throw new InputMismatchException("존재하지 않는 메뉴입니다.");
+                    }
+                }
             } catch (InputMismatchException e) {
                 System.out.println("존재하지 않는 메뉴입니다.");
             }
-       // }
+        }
     }
 }
