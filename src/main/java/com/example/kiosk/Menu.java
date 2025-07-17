28b2fda60dfb48fedb3ff7fe68147ea5797ce14c
Hyjpark <hyej042@gmail.com>
2025-07-17 18:07:14 +0900

    feat: 카테고리별 메뉴 초기화 구현

--- src/main/java/com/example/kiosk/Menu.java
+++ src/main/java/com/example/kiosk/Menu.java
@@ -5,12 +5,11 @@ import java.util.List;
 
 // MenuItem 클래스를 관리
 public class Menu {
+    String category;
     List<MenuItem> menuItems = new ArrayList<>();
 
-    Menu(List<List<String>> menuList) {
-        for (List<String> menu : menuList) {
-            menuItems.add(new MenuItem(menu.get(0), menu.get(1), menu.get(2)));
-        }
+    Menu(String category) {
+        this.category = category;
     }
 
     public void showMenuItem() {
