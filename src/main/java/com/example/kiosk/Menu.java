dd5642ca0f1c812d8fea189b331d41938b50c048
Hyjpark <hyej042@gmail.com>
2025-07-17 13:26:14 +0900

    feat: MenuItem을 순차적으로 보여주는 함수 추가

--- src/main/java/com/example/kiosk/Menu.java
+++ src/main/java/com/example/kiosk/Menu.java
@@ -12,4 +12,22 @@ public class Menu {
             menuItems.add(new MenuItem(menu.get(0), menu.get(1), menu.get(2)));
         }
     }
+
+    public void showMenuItem() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n[ SHAKESHACK MENU ]\n");
+        for (int  i = 0; i < menuItems.size(); i++) {
+            int blankLength = 12;
+            sb.append((i + 1) + ". " );
+            sb.append(menuItems.get(i).name);
+            sb.append(" ".repeat(blankLength - menuItems.get(i).name.length()) +  " | "); // 이름과 가격 사이의 공백 생성
+            sb.append("W " + menuItems.get(i).price +  " | ");
+            sb.append(menuItems.get(i).description);
+            sb.append("\n");
+        }
+        sb.append("0. 종료 \t| 종료");// 메뉴 출력
+
+        System.out.println(sb.toString());
+    }
+
 }
